588f2301cfa76db40d22b65368eee569623798a6
Kai Banto Eckert <y.banto@outlook.com>
2020-09-29 21:54:03 +0200

    Added train_1_clean database path

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 
 dirname: str = os.path.dirname(__file__)
 
-datapaths = {'tsa': {'train_1': normabspath(dirname, 'tsa/data/train_1.csv')}}
+datapaths = {'tsa': {'train_1': normabspath(dirname, 'tsa/data/train_1.csv'),
+                     'train_1_clean': normabspath(dirname, 'tsa/data/train_1_clean.csv')}}
