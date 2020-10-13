26606429518af847ee85ff749dc75a9efa167e52
Kai Banto Eckert <y.banto@outlook.com>
2020-10-13 11:24:14 +0200

    Creation

---
+++ tf2/config.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+
+def normabspath(basedir: str, filename: str):
+    return os.path.normpath(os.path.join(basedir, filename))
+
+
+dirname: str = os.path.dirname(__file__)
+
+datapaths = {'banknotes': normabspath(dirname, 'data/banknotes.csv'),
+             'darts': normabspath(dirname, 'data/darts.csv')}
+
